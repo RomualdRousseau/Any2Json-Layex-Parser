df9990f7e8ddecc2141e62029ed5d822372eeed1
Romuald Rousseau <romuald.rousseau@servier.com>
2024-03-11 18:55:28 +0800

    fix: better handling of merged cells

--- src/main/java/com/github/romualdrousseau/any2json/layex/TableLexer.java
+++ src/main/java/com/github/romualdrousseau/any2json/layex/TableLexer.java
@@ -11,9 +11,10 @@ public class TableLexer implements Lexer<BaseCell, TableLexer.Cursor> {
 
     class Cursor {
 
-        public Cursor(final int colIndex, final int rowIndex) {
+        public Cursor(final int colIndex, final int rowIndex, final int remaining) {
             this.colIndex = colIndex;
             this.rowIndex = rowIndex;
+            this.remaining = remaining;
         }
 
         public int getColIndex() {
@@ -24,8 +25,13 @@ public class TableLexer implements Lexer<BaseCell, TableLexer.Cursor> {
             return this.rowIndex;
         }
 
+        public int getRemaining() {
+            return this.remaining;
+        }
+
         private final int colIndex;
         private final int rowIndex;
+        private final int remaining;
     }
 
     public TableLexer(final Table table, int rowOffset) {
@@ -33,6 +39,7 @@ public class TableLexer implements Lexer<BaseCell, TableLexer.Cursor> {
         this.table = (BaseTable) table;
         this.colIndex = 0;
         this.rowIndex = rowOffset;
+        this.remaining = 0;
     }
 
     @Override
@@ -54,8 +61,20 @@ public class TableLexer implements Lexer<BaseCell, TableLexer.Cursor> {
             return BaseCell.EndOfRow;
         }
 
-        final BaseCell cell = row.getCellAt(colIndex);
-        colIndex += cell.getMergedCount();
+        BaseCell cell = row.getCellAt(this.colIndex);
+        final int n = cell.getMergedCount();
+
+        if (this.remaining > 0) {
+            cell = new BaseCell(cell.getValue(), this.colIndex + (n - this.remaining), n, this.table.getSheet());
+            this.remaining--;
+            if (this.remaining == 0) {
+                this.colIndex += n;
+            }
+        } else if (n > 1) {
+            this.remaining = n - 1;
+        } else {
+            this.colIndex++;
+        }
 
         return cell;
     }
@@ -80,7 +99,7 @@ public class TableLexer implements Lexer<BaseCell, TableLexer.Cursor> {
 
     @Override
     public void push() {
-        this.stack.add(new Cursor(this.colIndex, this.rowIndex));
+        this.stack.add(new Cursor(this.colIndex, this.rowIndex, this.remaining));
     }
 
     @Override
@@ -92,10 +111,12 @@ public class TableLexer implements Lexer<BaseCell, TableLexer.Cursor> {
     public void seek(final Cursor c) {
         this.colIndex = c.getColIndex();
         this.rowIndex = c.getRowIndex();
+        this.remaining = c.getRemaining();
     }
 
     private final ArrayList<Cursor> stack;
     private final BaseTable table;
     private int colIndex;
     private int rowIndex;
+    private int remaining;
 }
